0c725a3f691e23971637e60414783296428f3572
LimboHunter <user36976@st.guap.ru>
2023-11-28 00:14:30 +0300

    Started working on Admin app

---
+++ src/main/java/pizzas/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package pizzas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+        throws Exception{
+        http
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests.anyRequest().authenticated()
+                )
+                .oauth2Login(
+                        oauth2Login ->
+                                oauth2Login.loginPage("/oauth2/authorization/pizza-admin-client"))
+                .oauth2Client(withDefaults());
+        return http.build();
+    }
+}
