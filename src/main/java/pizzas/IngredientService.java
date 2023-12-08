e2671dcd6663c997dfcc79f9a72e16954a6d122b
LimboHunter <user36976@st.guap.ru>
2023-12-08 22:08:21 +0300

    finished with client but still have fall backs with redirection

---
+++ src/main/java/pizzas/IngredientService.java
@@ -0,0 +1,8 @@
+package pizzas;
+
+public interface IngredientService {
+
+    Iterable<Ingredient> findAll();
+
+    Ingredient addIngredient(Ingredient ingredient);
+}
--- src/main/java/pizzas/config/SecurityConfig.java
+++ src/main/java/pizzas/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package pizzas.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.context.annotation.RequestScope;
+import pizzas.IngredientService;
+import pizzas.RestIngredientService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -11,8 +20,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-        throws Exception{
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(
                         authorizeRequests -> authorizeRequests.anyRequest().authenticated()
@@ -23,4 +31,30 @@ public class SecurityConfig {
                 .oauth2Client(withDefaults());
         return http.build();
     }
+
+    @Bean
+    @RequestScope
+    public IngredientService ingredientService(
+            OAuth2AuthorizedClientService clientService) {
+        Authentication authentication =
+                SecurityContextHolder.getContext().getAuthentication();
+
+        String accessToken = null;
+
+        if (authentication.getClass()
+                .isAssignableFrom(OAuth2AuthenticationToken.class)) {
+            OAuth2AuthenticationToken oauthToken =
+                    (OAuth2AuthenticationToken) authentication;
+            String clientRegistrationId =
+                    oauthToken.getAuthorizedClientRegistrationId();
+            if (clientRegistrationId.equals("pizza-admin-client")) {
+                OAuth2AuthorizedClient client =
+                        clientService.loadAuthorizedClient(
+                                clientRegistrationId, oauthToken.getName());
+                accessToken = client.getAccessToken().getTokenValue();
+            }
+        }
+        return new RestIngredientService(accessToken);
+    }
+
 }
